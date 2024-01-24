d5489c9ddf7a1fda4f76e8f9247335772595bc18
Gosamat <61824985+Gosamat@users.noreply.github.com>
2024-01-24 16:15:35 +0000

    removed extra /

--- src/Pages/Contacts/Contacts.tsx
+++ src/Pages/Contacts/Contacts.tsx
@@ -17,16 +17,12 @@ export default function Contacts(){
         rel="noopener noreferrer">
         <img src="/images/linkedin-contact-icon.png"/>
       </a>
-      <a  href="/Gonçalo-CV-2024-Software-Developer.pdf"/"
+      <a  href="/Gonçalo-CV-2024-Software-Developer.pdf"
         target="_blank">
         <img src="/images/cv.png"/>
       </a>
     </div>
-    <hr />
+    <hr/>
     <h3 className='mt-5 text-xl text-charcoal-black'>© 2024 Gonçalo Matias</h3>
-
-</section>
-
-
-  );
+</section>);
 }
